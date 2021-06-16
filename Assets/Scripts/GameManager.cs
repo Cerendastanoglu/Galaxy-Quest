998f5f8518330c61265e8cea5c35de65a6fd6b8c
Ceren Dastanoglu <32312089+Cerendastanoglu@users.noreply.github.com>
2021-06-16 02:59:42 +0200

    Levels

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,21 +5,5 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public void LoadNextLevel(int x)
-    {
-        SceneManager.LoadScene(x);
-
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
